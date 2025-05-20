e5b0173c2a4a87004281eaf90a952e3e99d29b1e
owen newsome <melfordmalichai@gmail.com>
2025-05-20 12:32:01 +0900

    I did the show page and controller show action

--- app/controllers/furnitures_controller.rb
+++ app/controllers/furnitures_controller.rb
@@ -3,5 +3,22 @@ class FurnituresController < ApplicationController
   end
 
   def show
+    @furniture = Furniture.find(params[:id])
+  end
+
+  # def create
+  #   @furniture = Furniture.find([:id])
+  #   @furniture = Furniture.new(strong_params)
+  #   @furniture.rentals = @furniture
+  #   if @furniture.save
+  #     redirect_to @furnitures
+  #   else
+  #     @furniture = Furniture.all
+  #     render 'furnitures/show', status: :unprocessable_entity
+  #   end
+  # end
+
+  def strong_params
+    params.require(:furniture).permit(:name, :furniture_type, :price, :description)
   end
 end
